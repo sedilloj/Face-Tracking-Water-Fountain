3a341af7c4decfb515911dafa5e6fd6e68eba13d
Julian Sedillo <julianv.sedillo@gmail.com>
2023-06-16 05:20:27 -0400

    Partially implement aiming feature

--- main.py
+++ main.py
@@ -1,13 +1,59 @@
 # Import Libraries
 import RPi.GPIO as GPIO
+import time
 
 # Import files
 from servo import Servos
 from sprayer import Sprayer
 from camera import Camera
 
-x_middle = 320
-y_middle = 240
+X_CENTER = 320
+Y_CENTER = 240
+X_TOLERANCE = 50
+Y_TOLERANCE = 25
+MAX_WAITS = 4
+
+def calculate_rect_dist_to_center(coordRect):
+  coordRectCenter = ((coordRect[0] + coordRect[2]) / 2, (coordRect[1] + coordRect[3]) / 2)
+  return X_CENTER - coordRectCenter[0], Y_CENTER - coordRectCenter[1]
+
+def calculate_servoX_adjustment(diffX):
+  # This function will never be called if the center difference is between (-X_TOLERANCE, X_TOLERANCE)
+  if diffX < -300:
+    return -1
+  elif diffX < -200:
+    return -0.75
+  elif diffX < -100:
+    return -0.5
+  elif diffX < -X_TOLERANCE:
+    return -0.25
+  elif diffX < X_TOLERANCE:
+    return 0
+  elif diffX < 100:
+    return 0.25
+  elif diffX < 200:
+    return 0.5
+  elif diffX < 300:
+    return 0.75
+  else:
+    return 1
+  
+def calculate_servoY_adjustment(diffY):
+  # This function will never be called if the center difference is between (-Y_TOLERANCE, Y_TOLERANCE)
+  if diffY < -100:
+    return -0.75
+  elif diffY < -50:
+    return -0.5
+  elif diffY < -Y_TOLERANCE:
+    return -0.25
+  elif diffY < Y_TOLERANCE:
+    return 0
+  elif diffY < 50:
+    return 0.25
+  elif diffY < 100:
+    return 0.5
+  else:
+    return 0.75
 
 if __name__ == '__main__':
   # Set mode as BOARD
@@ -19,22 +65,43 @@ if __name__ == '__main__':
   camera = Camera()
 
   try:
+    waitCounter = 0
+
     # Always running
     while True:
-      rect_target = camera.query_expression()
+      if waitCounter == MAX_WAITS:
+        waitCounter = 0
+        # reset the servos to starting position
 
-      if not len(rect_target):
-        # Search if matching face undetected
-        servos.search_step()
+      rectTarget = camera.query_expression()
+
+      if not len(rectTarget):
+        if waitCounter:
+          # Wait to re-detect face if adjusting made it undetectable
+          waitCounter += 1
+          time.sleep(1.0)
+          continue
+
+        else:
+          # Search if matching face undetected
+          servos.search_step()
+
       else:
         # Aim to face if face is detected
-        diffX = x_middle # adjust this code later
+        distXToCenter, distYToCenter = calculate_rect_dist_to_center(rectTarget)
+
+        adjustX = calculate_servoX_adjustment(distXToCenter)
+        adjustY = calculate_servoY_adjustment(distYToCenter)
+
+        if not adjustX and not adjustY:
+          # spray
+          print('spraying')
+        else:
+          print('adjust')
+
+          
         
   finally:
     # Clean up
-    servos.servoX.stop()
-    servos.servoY.stop()
     GPIO.cleanup()
-
-  # set servos to refine position to aim towards the face
   
